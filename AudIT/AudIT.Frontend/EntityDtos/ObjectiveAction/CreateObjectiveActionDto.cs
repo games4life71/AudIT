2d7b7058cba6d156f9707f935e36ad32fd0d607b
Dogaru Stefan <7stefanadrian@gmail.com>
2024-06-02 19:30:58 +0300

    designed the Create Objective Page

---
+++ AudIT/AudIT.Frontend/EntityDtos/ObjectiveAction/CreateObjectiveActionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Frontend.EntityDtos.ObjectiveAction;
+
+public class CreateObjectiveActionDto
+{
+    public string Name { get; set; }
+
+    public Guid ObjectiveId { get; set; }
+
+    public CreateObjectiveActionDto()
+    {
+        // Name = string.Empty;
+        Name = "name 124";
+            ObjectiveId = Guid.Empty;
+    }
+}
