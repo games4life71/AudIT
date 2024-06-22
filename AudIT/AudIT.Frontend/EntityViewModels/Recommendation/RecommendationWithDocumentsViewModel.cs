325605251b4b47ffba638b6f75028fbcd0484453
Dogaru Stefan <7stefanadrian@gmail.com>
2024-06-23 00:45:35 +0300

    implemented auditor can see uploaded documents for an audit mission

---
+++ AudIT/AudIT.Frontend/EntityViewModels/Recommendation/RecommendationWithDocumentsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Frontend.EntityViewModels.Documents;
+using Frontend.EntityViewModels.ObjectiveAction;
+
+namespace Frontend.EntityViewModels.Recommendation;
+
+public class RecommendationWithDocumentsViewModel
+{
+    public BaseRecommendationViewModel Recommendation { get; set; }
+
+    public List<BaseDocumentViewModel> Documents { get; set; }
+}
